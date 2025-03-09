427e9e8e4c9be2b7794b79eab82d2f0ba8fa9378
Dostan <turlybekdostan@gmail.com>
2025-03-09 18:22:29 +0500

    [Model] Add model for location and smart id for them

---
+++ MapApLeraning/Model/LocationModel.swift
@@ -0,0 +1,34 @@
+import Foundation
+import MapKit
+
+struct Location: Identifiable{
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+    //when we create many places in the same place, we create diferent id also
+    // we need that in the same places was same id
+    //I dont know why we normaly need this yet :)
+    //Identefible
+    var id: String{
+        name + cityName
+    }
+}
+
+///This is location struct:
+/*
+Location(
+    name: "Trevi Fountain",
+    cityName: "Rome",
+    coordinates: CLLocationCoordinate2D(latitude: 41.9009, longitude: 12.4833),
+    description: "The Trevi Fountain is a fountain in the Trevi district in Rome, Italy, designed by Italian architect Nicola Salvi and completed by Giuseppe Pannini and several others. Standing 26.3 metres high and 49.15 metres wide, it is the largest Baroque fountain in the city and one of the most famous fountains in the world.",
+    imageNames: [
+        "rome-trevifountain-1",
+        "rome-trevifountain-2",
+        "rome-trevifountain-3",
+    ],
+    link: "https://en.wikipedia.org/wiki/Trevi_Fountain"),
+*/
