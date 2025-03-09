e58c9d05b05fe39c0d486b576aadb17213bb1949
Dostan <turlybekdostan@gmail.com>
2025-03-09 22:27:47 +0500

    [ViewModel] Add logic for update current location

--- MapApLeraning/ViewModel/LocationViewModel.swift
+++ MapApLeraning/ViewModel/LocationViewModel.swift
@@ -1,11 +1,34 @@
 import SwiftUI
-
+import MapKit
 
 class LocationViewModel: ObservableObject {
+    
+    //All loaded locatoins
     @Published var locations: [Location]
+    
+    //Current Location on map
+    @Published var mapLocation: Location{
+        didSet{
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init(){
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    public func updateMapRegion(location: Location){
+        withAnimation (.easeInOut){
+            mapRegion = MKCoordinateRegion(
+                center: location.coordinates,
+                span: mapSpan
+            )
+        }
     }
 }
