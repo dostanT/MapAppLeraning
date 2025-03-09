08ca8f565c1a630a492e614d26dac6a1d074a6d4
Dostan <turlybekdostan@gmail.com>
2025-03-09 15:44:15 +0500

    Initial Commit

---
+++ MapApLeraning/MapApLeraningApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApLeraningApp.swift
+//  MapApLeraning
+//
+//  Created by Dostan Turlybek on 09.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct MapApLeraningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
