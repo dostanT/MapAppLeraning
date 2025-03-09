bb04d4bb977f7e8c7a5f80c9607ddfb5f13754bf
Dostan <turlybekdostan@gmail.com>
2025-03-09 22:25:56 +0500

    [View] Remove statical varibal and create First Map

--- MapApLeraning/View/LocationView.swift
+++ MapApLeraning/View/LocationView.swift
@@ -1,14 +1,13 @@
 import SwiftUI
+import MapKit
 
 struct LocationView: View {
     
     @EnvironmentObject private var vm: LocationViewModel
     
     var body: some View{
-        List{
-            ForEach(vm.locations){i in
-                Text("\(i.name)")
-            }
+        ZStack{
+            Map(coordinateRegion: $vm.mapRegion)
         }
     }
 }
