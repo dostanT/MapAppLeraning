3bfa6266d4c05205486a23b6726c1d7857a0da3a
Dostan <turlybekdostan@gmail.com>
2025-03-10 13:43:36 +0500

    [VIEW] create animation for: title; arrow : and new interface

--- MapApLeraning/View/LocationView.swift
+++ MapApLeraning/View/LocationView.swift
@@ -8,7 +8,52 @@ struct LocationView: View {
     var body: some View{
         ZStack{
             Map(coordinateRegion: $vm.mapRegion)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 0){
+                header
+                    .padding()
+                
+                Spacer()
+            }
         }
+    }
+}
+
+extension LocationView{
+    private var header: some View{
+        VStack {
+            Button{
+                vm.toggleLocationList()
+            }label:{
+                Text("\(vm.mapLocation.name + ", " + vm.mapLocation.cityName)")
+                    .font(.title2)
+                    .fontWeight(.black)
+                    .foregroundStyle(.primary)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+//this animation with numericText i like more than animation which show me Nick Sarno
+                    .contentTransition(.numericText())
+                //this is nickSarno Method
+//                    .animation(.easeInOut, value: vm.mapLocation)
+                    .overlay(alignment: .leading) {
+                        Image(systemName: "arrow.down")
+                            .rotationEffect(Angle(degrees: vm.showListLocations ? -180 : 0))
+                            .font(.headline)
+                            .foregroundStyle(.primary)
+                            .padding()
+                    }
+            }
+            
+                
+            if vm.showListLocations{
+                LocationsListView()
+            }
+            
+        }
+        .background(.thinMaterial)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
     }
 }
 
