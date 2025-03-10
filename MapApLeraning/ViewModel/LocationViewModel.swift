d0e2e8ddf276e794e8a8be0cc5475a24326ce0ed
Dostan <turlybekdostan@gmail.com>
2025-03-10 13:45:53 +0500

    [VIEWMODEL] add variable for animaition and create func for toggle and another func for show next location with animation

--- MapApLeraning/ViewModel/LocationViewModel.swift
+++ MapApLeraning/ViewModel/LocationViewModel.swift
@@ -12,9 +12,12 @@ class LocationViewModel: ObservableObject {
             updateMapRegion(location: mapLocation)
         }
     }
-    
+    // Current Region on map
     @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
-    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+    
+    //Show list of locations
+    @Published var showListLocations: Bool = false
     
     init(){
         let locations = LocationsDataService.locations
@@ -23,12 +26,24 @@ class LocationViewModel: ObservableObject {
         self.updateMapRegion(location: locations.first!)
     }
     
-    public func updateMapRegion(location: Location){
+    private func updateMapRegion(location: Location){
         withAnimation (.easeInOut){
             mapRegion = MKCoordinateRegion(
                 center: location.coordinates,
                 span: mapSpan
             )
+        }
+    }
+    public func toggleLocationList(){
+        withAnimation(.easeInOut){
+            showListLocations.toggle()
+        }
+    }
+    
+    public func showNextLocation(location: Location){
+        withAnimation(.easeInOut){
+            mapLocation = location
+            showListLocations = false
         }
     }
 }
